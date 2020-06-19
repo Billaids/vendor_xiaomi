3612691450b761578afbebfa0d7e5845cd2e0242
billaids <jimmy.nelle@hsw-stud.de>
2020-06-19 10:07:10 +0200

    ginkgo: Update ACDB from GINKGO_20.6.17_50f25d9985_10.0
    Signed-off-by: billaids <jimmy.nelle@hsw-stud.de>

--- ginkgo/ginkgo-vendor.mk
+++ ginkgo/ginkgo-vendor.mk
@@ -304,6 +304,23 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/IDP_Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/IDP_Headset_cal.acdb \
     vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/IDP_Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/IDP_Speaker_cal.acdb \
     vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/IDP_workspaceFile.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/IDP_workspaceFile.qwsp \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_Bluetooth_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_General_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_Global_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_Handset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_Handset_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_Hdmi_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_Hdmi_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_Headset_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_Speaker_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_workspaceFile.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tasha-snd-card/IDP_WCD9335_workspaceFile.qwsp \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Bluetooth_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Codec_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Codec_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_General_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Global_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Handset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Handset_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Hdmi_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Hdmi_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Headset_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_Speaker_cal.acdb \
+    vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_workspaceFile.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/IDP/trinket-tavil-snd-card/IDP_WCD9340_workspaceFile.qwsp \
     vendor/xiaomi/ginkgo/proprietary/vendor/etc/acdbdata/adsp_avs_config.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/adsp_avs_config.acdb \
     vendor/xiaomi/ginkgo/proprietary/vendor/etc/cacert_location.pem:$(TARGET_COPY_OUT_VENDOR)/etc/cacert_location.pem \
     vendor/xiaomi/ginkgo/proprietary/vendor/etc/camera/CFR_para.bin:$(TARGET_COPY_OUT_VENDOR)/etc/camera/CFR_para.bin \
